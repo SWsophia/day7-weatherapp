faf813622c1a5081f7feb80330486f5c3d7d46cb
Sophia Sun <ddiscovery@live.com>
2022-02-10 14:56:58 +0800

    all

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { CityListComponent } from './components/city-list.component';
+import { WeatherComponent } from './components/weather.component';
+import { WeatherService } from './weather.service';
+
+const appRoutes: Routes = [
+{ path: '', component: CityListComponent},
+{ path: 'weather/:city', component: WeatherComponent}
+
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    CityListComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, { useHash: true })
+  ],
+  providers: [WeatherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
